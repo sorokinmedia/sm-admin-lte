12343fc39edc9087841f48b30505c1943031fc3c
magarampage <magarampage@gmail.com>
2018-10-13 15:36:20 +0300

    SCRUM-2988

--- src/AppWrapper/index.js
+++ src/AppWrapper/index.js
@@ -85,21 +85,30 @@ export default class AppWrapper extends Component {
 						</div>
 						{jivosite
 							? (
-								<script type='text/javascript'>
-									(function(){ var widget_id = 'm7KxeJ82jO';var d=document;var w=window;function l()
+								(function () {
+									var widget_id = 'm7KxeJ82jO'
+									var d = document
+									var w = window
 
-								{var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true;s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);}
-									if(d.readyState=='complete')
+									function l() {
+										var s = document.createElement('script')
+										s.type = 'text/javascript'
+										s.async = true
+										s.src = '//code.jivosite.com/script/widget/' + widget_id
+										var ss = document.getElementsByTagName('script')[0]
+										ss.parentNode.insertBefore(s, ss)
+									}
 
-								{l();}
-									else{if(w.attachEvent)
-
-								{w.attachEvent('onload',l);}
-									else
-
-								{w.addEventListener('load',l,false);}
-								}})();
-								</script>
+									if (d.readyState == 'complete') {
+										l()
+									} else {
+										if (w.attachEvent) {
+											w.attachEvent('onload', l)
+										} else {
+											w.addEventListener('load', l, false)
+										}
+									}
+								})()
 							) : null}
 					</Footer>
 					<SidebarRight
